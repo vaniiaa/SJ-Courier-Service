3c301de31612b9dc5206b790a0a3295d90c559a6
vaniiaa <vanivania2005@gmail.com>
2025-04-26 20:02:16 +0700

    update logo admin

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -5,7 +5,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title', 'SJ Courier - Admin')</title>
-
+    <link rel="icon" href="{{ asset('images/admin/logo2.jpg') }}" type="image/jpeg">
+    
     {{-- Tailwind CSS CDN --}}
     <script src="https://cdn.tailwindcss.com"></script>
 
--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -4,7 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title')</title>
-
+    <link rel="icon" href="{{ asset('images/admin/logo2.jpg') }}" type="image/jpeg">
+    
     <!-- Tailwind CSS -->
     <script src="https://cdn.tailwindcss.com"></script>
     <script>
