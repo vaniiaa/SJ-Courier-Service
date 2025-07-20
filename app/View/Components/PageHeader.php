5be769770836bf5810af4332abb42e3cef9ac53b
ValuhkJv <valuhkjv@gmail.com>
2025-07-20 16:43:27 +0700

    Perbaikan

--- app/View/Components/PageHeader.php
+++ app/View/Components/PageHeader.php
@@ -2,24 +2,34 @@
 
 namespace App\View\Components;
 
-use Closure;
-use Illuminate\Contracts\View\View;
 use Illuminate\View\Component;
 
 class PageHeader extends Component
 {
     /**
-     * Create a new component instance.
+     * Judul halaman yang akan ditampilkan.
+     *
+     * @var string
      */
-    public function __construct()
+    public string $title;
+
+    /**
+     * Create a new component instance.
+     *
+     * @param  string  $title Judul halaman, defaultnya 'Halaman'.
+     * @return void
+     */
+    public function __construct(string $title = 'Halaman')
     {
-        //
+        $this->title = $title;
     }
 
     /**
      * Get the view / contents that represent the component.
+     *
+     * @return \Illuminate\Contracts\View\View|\Closure|string
      */
-    public function render(): View|Closure|string
+    public function render()
     {
         return view('components.page-header');
     }
